947a99b48647ed805690c290527a59d41cf5b96c
rmdhfz <hfzrmd@gmail.com>
2020-03-19 23:17:53 +0700

    initial commit

---
+++ maxofint.cpp
@@ -0,0 +1,9 @@
+#include <climits>
+#include <iostream>
+using namespace std;
+int main(){
+	int imin = INT_MIN;
+	int imax = INT_MAX;
+	cout<<"Min of int "<<imin<<endl;
+	cout<<"Max of int "<<imax<<endl; 
+}
